0e62f63a0194384be3e2fea228ddfa3c8fbb2ef6
JTong <eldertong@gmail.com>
2017-07-28 08:54:23 -0400

    helping marv with js fizzbuzz

--- public/js/fizz.js
+++ public/js/fizz.js
@@ -1,29 +1,29 @@
   function fizzbuzz() {
       var num = document.getElementById("choice").value;
-        if (num % 15 == 0)
-          alert(num+ " IS " + "MinedMinds!!");
-        else if (num % 3 == 0)
-          alert(num+ " IS " + "Mined!!");
-        else if (num % 5 == 0)
-          alert(num+ " IS " + "Minds!!");
-        else
-          alert("Sorry"+" " + num + "" + " is just " + num); 
-    } 
+      if (num % 15 == 0)
+          alert(num + " IS " + "MinedMinds!!");
+      else if (num % 3 == 0)
+          alert(num + " IS " + "Mined!!");
+      else if (num % 5 == 0)
+          alert(num + " IS " + "Minds!!");
+      else
+          alert("Sorry" + " " + num + "" + " is just " + num);
+  }
 
-function showanswer() {
-  var num  = 1;
-    while (num < 100)
-    {
-        if (num % 15 == 0) { document.write ("MinedMinds"); 
-        }
-        else if (num % 5 == 0) { document.write ("Minds"); 
-        }
-        else if (num % 3 == 0) { document.write ("Mined");
-        }
-        else{ document.write (num);
-        }
-        document.write ("<br>");
-        num += 1;
-    }
-}
-  
+  function showanswer() {
+      // alert("before");
+      var num = 1;
+      while (num < 101) {
+          if (num % 15 == 0) {
+              document.getElementById('answerhere').innerHTML += '<p>MinedMinds</p>'
+          } else if (num % 5 == 0) {
+              document.getElementById('answerhere').innerHTML += "<p>Minds</p>";
+          } else if (num % 3 == 0) {
+              document.getElementById('answerhere').innerHTML += "<p>Mined</p>";
+          } else {
+              document.getElementById('answerhere').innerHTML += '<p>' + num + '</p>';
+          }
+          num += 1;
+          // alert("after");
+      }
+  }
